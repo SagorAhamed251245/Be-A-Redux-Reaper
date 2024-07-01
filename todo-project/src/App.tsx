99e74d806f5533923ecef1f621cb36c32815f82f
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-07-01 16:33:49 +0600

    seadcn install

---
+++ todo-project/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>My todo</h1>
+      <Button style={{ margin: "10px" }}>This is button</Button>
+    </>
+  );
+}
+
+export default App;
